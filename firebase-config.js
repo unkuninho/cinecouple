dd58b78bfd77964b8586f2780672eb88a60c2b12
unkuninho <139827219+unkuninho@users.noreply.github.com>
2026-07-19 13:09:15 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,12 +7,12 @@
 // ============================================================
 
 const firebaseConfig = {
-  apiKey: "COLE_AQUI_SUA_API_KEY",
-  authDomain: "COLE_AQUI.firebaseapp.com",
-  projectId: "COLE_AQUI_SEU_PROJECT_ID",
-  storageBucket: "COLE_AQUI.appspot.com",
-  messagingSenderId: "COLE_AQUI",
-  appId: "COLE_AQUI"
+  apiKey: "AIzaSyCmD1aHSFFrmRGE13ZR7I99__b29DhdCkA",
+  authDomain: "goal-tracker-3f954.firebaseapp.com",
+  projectId: "goal-tracker-3f954",
+  storageBucket: "goal-tracker-3f954.firebasestorage.app",
+  messagingSenderId: "675317109213",
+  appId: "1:675317109213:web:b9792bf406a18995722634"
 };
 
 firebase.initializeApp(firebaseConfig);
